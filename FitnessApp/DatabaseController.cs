5310c5e38713f43fd75cacd90444ea866835fb71
Yggdrasil <jfc14cmu@uea.ac.uk>
2018-09-09 00:21:48 +0100

    Started implementing DatabaseController class

---
+++ FitnessApp/DatabaseController.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Diagnostics;
+
+namespace FitnessApp
+{
+    class DatabaseController
+    {
+        private const string HOST = "http://ec2-18-221-180-80.us-east-2.compute.amazonaws.com:3000";
+        private const string DATE_FORMAT = "yyyy-MM-dd";
+        private const string KEY_PASSWORD = "password";
+        private const string KEY_RECORD_TYPE = "recordType";
+        private const string KEY_USERNAME = "username";
+        private const string MOZILLA = "Mozilla/5.0";
+        //find date formatter
+        private const string USER_AGENT = "User-Agent";
+
+        public static string BODYWEIGHT_RECORD = "BodyweightRecord";
+        public static string BODYWEIGHT_RECORD_LIST = "bodyweightRecords";
+        public static string CALORIE_RECORD = "CalorieRecord";
+        public static string CALORIE_RECORD_LIST = "calorieRecords";
+        public static string EXERCISE = "Exercise";
+        public static string EXERCISE_LIST = "exercises";
+        public static string EXERCISE_RECORD = "ExerciseRecord";
+        public static string EXERCISE_RECORD_LIST = "exerciseRecords";
+
+        private string username;
+        private string password;
+
+        DatabaseController() { }
+
+        private HttpWebRequest establishRequest(string requestUrl, string method) {
+            try {
+                string uri = HOST + requestUrl;
+                Console.WriteLine(uri);
+                HttpWebRequest req = (HttpWebRequest)WebRequest.Create(uri);
+                req.UserAgent = MOZILLA;
+                req.Credentials = CredentialCache.DefaultCredentials;
+                req.ContentType = "application/x-www-form-urlencoded";
+                req.Method = method;
+                return req;
+            } catch (Exception e) { Console.WriteLine(e.ToString()); }
+            return null;
+        }
+        private bool resolveStatusCode(HttpWebResponse response) {
+            HttpStatusCode code = response.StatusCode;
+            if (code == HttpStatusCode.Accepted || code == HttpStatusCode.OK) { return true; }
+            else {
+                Console.WriteLine(code);
+                Console.WriteLine(response.StatusDescription);
+            }
+            return false;
+        }
+        private bool streamJObject(JObject o, HttpWebRequest req) {
+            try
+            {
+                byte[] bytes = Encoding.UTF8.GetBytes(o.ToString());
+                req.ContentType = (bytes.Length).ToString();
+                req.GetRequestStream().Write(bytes, 0, bytes.Length);
+                req.GetRequestStream().Close();
+                return true;
+            }
+            catch (Exception e) {
+                Console.WriteLine(e.ToString());
+            }
+            return false;
+        }
+
+        public bool Login(string username, string password) {
+            try
+            {
+                HttpWebRequest req = establishRequest("/login",WebRequestMethods.Http.Post);
+                JObject o = new JObject(
+                    new JProperty(KEY_USERNAME, username),
+                    new JProperty(KEY_PASSWORD, password)
+                    );
+                streamJObject(o,req);
+                HttpWebResponse res = (HttpWebResponse)req.GetResponse();
+                if (resolveStatusCode(res)) {
+                    this.username = username;
+                    this.password = password;
+                    res.Close();
+                    return true;
+                }
+                res.Close();
+            }
+            catch (Exception e) {
+                Console.WriteLine(e.ToString());
+            }
+            return false;
+        }
+
+        public bool AddUser(string username, string password) {
+            try {
+                HttpWebRequest req = establishRequest("/add/user/",WebRequestMethods.Http.Post);
+                JObject user = new JObject(
+                    new JProperty(KEY_USERNAME, username),
+                    new JProperty(KEY_PASSWORD, password)
+                    );
+                streamJObject(user,req);
+                HttpWebResponse res = (HttpWebResponse)req.GetResponse();
+                if (resolveStatusCode(res)) {
+                    res.Close();
+                    return true;
+                }
+                
+            } catch (Exception e) { Console.WriteLine(e.ToString()); }
+            return false;
+        }
+
+        public bool RemoveUser() {
+            try {
+                HttpWebRequest req = establishRequest("/remove/user/","DELETE");
+                Console.WriteLine(this.username);
+                Console.WriteLine(this.password);
+                JObject user = new JObject(
+                    new JProperty(KEY_USERNAME,this.username),
+                    new JProperty(KEY_PASSWORD,this.password)
+                    );
+                streamJObject(user,req);
+                HttpWebResponse res = (HttpWebResponse)req.GetResponse();
+                if (resolveStatusCode(res)) {
+                    res.Close();
+                    return true;
+                }
+            } catch (Exception e) { Console.WriteLine(e.ToString()); }
+            return false;
+        }
+        public bool UpdatePassword(string newPassword, string confirmedPassword) {
+            try {
+                HttpWebRequest req = establishRequest("/update/password",WebRequestMethods.Http.Put);
+                JObject o = new JObject(
+                    new JProperty(KEY_USERNAME,this.username),
+                    new JProperty("oldPassword",this.password),
+                    new JProperty("newPassword",newPassword),
+                    new JProperty("confirmedPassword",confirmedPassword)
+                    );
+                streamJObject(o, req);
+                HttpWebResponse res = (HttpWebResponse)req.GetResponse();
+                if (resolveStatusCode(res)) {
+                    this.password = newPassword;
+                    res.Close();
+                    return true;
+                }
+            } catch (Exception e) { Console.WriteLine(e.ToString()); }
+            return false;
+        }
+
+        public bool AddRecord(string rType, object record) {
+            try {
+                HttpWebRequest req = establishRequest("/add/",WebRequestMethods.Http.Post);
+                JObject o = RecordToJObject(record);
+                o.Add(KEY_USERNAME,this.username);
+                o.Add(KEY_PASSWORD,this.password);
+                o.Add("recordType",rType);
+                streamJObject(o,req);
+                HttpWebResponse res = (HttpWebResponse)req.GetResponse();
+                if (resolveStatusCode(res)) {
+                    res.Close();
+                    return true;
+                }
+            } catch(Exception e){ Console.WriteLine(e.ToString()); }
+            return false;
+        }
+
+        private JObject RecordToJObject(object o) {
+            if (o is Exercise) {
+                return new JObject(
+                    new JProperty("id",((Exercise)o).getId()),
+                    new JProperty("name",((Exercise)o).getName()),
+                    new JProperty("date",((Exercise)o).getRecordDate().ToString(DATE_FORMAT))
+                    );
+            }
+            else if (o is ExerciseRecord) {
+                return new JObject(
+                    new JProperty("id",((ExerciseRecord)o).getId()),
+                    new JProperty("exerciseID", ((ExerciseRecord)o).getExerciseId()),
+                    new JProperty("weight",((ExerciseRecord)o).getWeight()),
+                    new JProperty("date",((ExerciseRecord)o).getRecordDate().ToString(DATE_FORMAT))
+                    );
+            }
+            else if (o is CalorieRecord) {
+                return new JObject(
+                    new JProperty("id", ((CalorieRecord)o).getId()),
+                    new JProperty("calories",((CalorieRecord)o).getCalories()),
+                    new JProperty("date",((CalorieRecord)o).getRecordDate().ToString(DATE_FORMAT))
+                    );
+            }
+            else if (o is BodyweightRecord) {
+                return new JObject(
+                    new JProperty("id",((BodyweightRecord)o).getId()),
+                    new JProperty("weight",((BodyweightRecord)o).getWeight()),
+                    new JProperty("date", ((BodyweightRecord)o).getRecordDate().ToString(DATE_FORMAT))
+                    );
+            }
+            return null;
+        }
+
+        static void Main() {
+            DatabaseController dc = new DatabaseController();
+            //Trace.Assert(dc.AddUser("test1","pass"),"Failed to add user!");
+            bool loggedIn = dc.Login("test1","pass");
+            Trace.Assert(loggedIn == true,"Failed to login");
+            //Trace.Assert(dc.UpdatePassword("pass","pass"),"Failed to update password");
+            //Trace.Assert(dc.RemoveUser(),"Failed to remove user");
+            Exercise ex = new Exercise(1,"Dumbell Shrug",DateTime.Now);
+            Trace.Assert(dc.AddRecord(EXERCISE,ex),"Failed to add record");
+
+            
+        }
+    }
+    
+}
--- FitnessApp/Program.cs
+++ FitnessApp/Program.cs
@@ -11,22 +11,24 @@ namespace FitnessApp
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            //Application.EnableVisualStyles();
-            //Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new Form1());
+        //[STAThread]
+        //static void Main()
+        //{
+        //    //Application.EnableVisualStyles();
+        //    //Application.SetCompatibleTextRenderingDefault(false);
+        //    //Application.Run(new Form1());
 
-            Exercise ex = new Exercise(10,"DB Chest Press", DateTime.Now);
-            ExerciseRecord exr = new ExerciseRecord(1, 10, 34.0, DateTime.Now);
-            CalorieRecord cr = new CalorieRecord(5, 1050, DateTime.Now);
-            BodyweightRecord bwr = new BodyweightRecord(3, 80.5, DateTime.Now);
-            Console.WriteLine(ex);
-            Console.WriteLine(exr);
-            Console.WriteLine(cr);
-            Console.WriteLine(bwr);
+        //    //Exercise ex = new Exercise(10,"DB Chest Press", DateTime.Now);
+        //    //ExerciseRecord exr = new ExerciseRecord(1, 10, 34.0, DateTime.Now);
+        //    //CalorieRecord cr = new CalorieRecord(5, 1050, DateTime.Now);
+        //    //BodyweightRecord bwr = new BodyweightRecord(3, 80.5, DateTime.Now);
+        //    //Console.WriteLine(ex);
+        //    //Console.WriteLine(exr);
+        //    //Console.WriteLine(cr);
+        //    //Console.WriteLine(bwr);
 
-        }
+        //    DatabaseController dc = new DatabaseController();
+        //    dc.Login("user1","p123");
+        //}
     }
 }
