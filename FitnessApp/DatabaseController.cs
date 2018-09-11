be3f393665a8ebbf4157b7c51e70d5bfd18b38a3
Yggdrasil <jfc14cmu@uea.ac.uk>
2018-09-11 21:01:25 +0100

    Finished the DatabaseController class and made a start on the ModelController

--- FitnessApp/DatabaseController.cs
+++ FitnessApp/DatabaseController.cs
@@ -22,18 +22,18 @@ namespace FitnessApp
         private const string USER_AGENT = "User-Agent";
 
         public static string BODYWEIGHT_RECORD = "BodyweightRecord";
-        public static string BODYWEIGHT_RECORD_LIST = "bodyweightRecords";
+        public const string BODYWEIGHT_RECORD_LIST = "bodyweightRecords";
         public static string CALORIE_RECORD = "CalorieRecord";
-        public static string CALORIE_RECORD_LIST = "calorieRecords";
+        public const string CALORIE_RECORD_LIST = "calorieRecords";
         public static string EXERCISE = "Exercise";
-        public static string EXERCISE_LIST = "exercises";
+        public const string EXERCISE_LIST = "exercises";
         public static string EXERCISE_RECORD = "ExerciseRecord";
-        public static string EXERCISE_RECORD_LIST = "exerciseRecords";
+        public const string EXERCISE_RECORD_LIST = "exerciseRecords";
 
         private string username;
         private string password;
 
-        DatabaseController() { }
+        public DatabaseController() { }
 
         private HttpWebRequest establishRequest(string requestUrl, string method) {
             try {
@@ -70,6 +70,56 @@ namespace FitnessApp
                 Console.WriteLine(e.ToString());
             }
             return false;
+        }
+        private JObject RecordToJObject(object o)
+        {
+            if (o is Exercise)
+            {
+                return new JObject(
+                    new JProperty("id", ((Exercise)o).getId()),
+                    new JProperty("name", ((Exercise)o).getName()),
+                    new JProperty("date", ((Exercise)o).getRecordDate().ToString(DATE_FORMAT))
+                    );
+            }
+            else if (o is ExerciseRecord)
+            {
+                return new JObject(
+                    new JProperty("id", ((ExerciseRecord)o).getId()),
+                    new JProperty("exerciseID", ((ExerciseRecord)o).getExerciseId()),
+                    new JProperty("weight", ((ExerciseRecord)o).getWeight()),
+                    new JProperty("date", ((ExerciseRecord)o).getRecordDate().ToString(DATE_FORMAT))
+                    );
+            }
+            else if (o is CalorieRecord)
+            {
+                return new JObject(
+                    new JProperty("id", ((CalorieRecord)o).getId()),
+                    new JProperty("calories", ((CalorieRecord)o).getCalories()),
+                    new JProperty("date", ((CalorieRecord)o).getRecordDate().ToString(DATE_FORMAT))
+                    );
+            }
+            else if (o is BodyweightRecord)
+            {
+                return new JObject(
+                    new JProperty("id", ((BodyweightRecord)o).getId()),
+                    new JProperty("weight", ((BodyweightRecord)o).getWeight()),
+                    new JProperty("date", ((BodyweightRecord)o).getRecordDate().ToString(DATE_FORMAT))
+                    );
+            }
+            return null;
+        }
+
+        private string getResponseData(HttpWebResponse res)
+        {
+            Stream stream = res.GetResponseStream();
+            StreamReader streamReader = new StreamReader(stream);
+            string line = "";
+            string data = "";
+            while ((line = streamReader.ReadLine()) != null)
+            {
+                data += line;
+            }
+            return data;
         }
 
         public bool Login(string username, string password) {
@@ -168,51 +218,219 @@ namespace FitnessApp
             } catch(Exception e){ Console.WriteLine(e.ToString()); }
             return false;
         }
+        
+        public bool DeleteRecord(string rType, object record) {
+            try {
+                HttpWebRequest req = establishRequest("/remove/","DELETE");
+                JObject o = RecordToJObject(record);
+                o.Add(KEY_USERNAME, username);
+                o.Add(KEY_PASSWORD, password);
+                o.Add(KEY_RECORD_TYPE, rType);
+                streamJObject(o,req);
+                HttpWebResponse res = (HttpWebResponse)req.GetResponse();
+                if (resolveStatusCode(res)) {
+                    res.Close();
+                    return true;
+                }
+            } catch (Exception e) { Console.WriteLine(e.ToString()); }
+            return false;
+        }
+        public bool EditRecord(string rType, object record) {
+            try {
+                HttpWebRequest req = establishRequest("/update/",WebRequestMethods.Http.Put);
+                JObject o = RecordToJObject(record);
+                o.Add(KEY_USERNAME, username);
+                o.Add(KEY_PASSWORD, password);
+                o.Add(KEY_RECORD_TYPE, rType);
+                streamJObject(o,req);
+                HttpWebResponse res = (HttpWebResponse)req.GetResponse();
+                if (resolveStatusCode(res)) {
+                    res.Close();
+                    return true;
+                }
+            } catch (Exception e) { Console.WriteLine(e.ToString()); }
+            return false;
+        }
+        //NEEDS TESTING
+        public int GenerateID(string rType) {
+            try {
+                string getUrl = "/" + username + "/" + password + "/generate/" + rType + "/id";
+                HttpWebRequest req = establishRequest(getUrl,WebRequestMethods.Http.Get);
+                HttpWebResponse res = (HttpWebResponse)req.GetResponse();
+                if (resolveStatusCode(res)) {
+                    string data = getResponseData(res);
+                    int id = Convert.ToInt32(data);
+                    res.Close();
+                    return id;
+                }
+            } catch (Exception e) { Console.WriteLine(e.ToString()); }
+            return -1;
+        }
 
-        private JObject RecordToJObject(object o) {
-            if (o is Exercise) {
-                return new JObject(
-                    new JProperty("id",((Exercise)o).getId()),
-                    new JProperty("name",((Exercise)o).getName()),
-                    new JProperty("date",((Exercise)o).getRecordDate().ToString(DATE_FORMAT))
-                    );
+        //NEEDS TESTING
+        public int GetID(string rType, string name) {
+            try {
+                string getUrl = "/getID/" + username + "/" + password + "/" + name + "/" + rType;
+                HttpWebRequest req = establishRequest(getUrl, WebRequestMethods.Http.Get);
+                HttpWebResponse res = (HttpWebResponse)req.GetResponse();
+                if (resolveStatusCode(res)) {
+                    string data = getResponseData(res);
+                    int id = Convert.ToInt32(data);
+                    res.Close();
+                    return id;
+                }
+            } catch (Exception e) { Console.WriteLine(e.ToString()); }
+            return -1;
+        }
+
+        public Exercise[] GetExerciseList() {
+            object[] list = GetRecordList(EXERCISE_LIST);
+            if (list == null) { return new Exercise[0]; }
+            Exercise[] exList = new Exercise[list.Length];
+            for (int i = 0; i < list.Length; i++) {
+                exList[i] = (Exercise)list[i];
             }
-            else if (o is ExerciseRecord) {
-                return new JObject(
-                    new JProperty("id",((ExerciseRecord)o).getId()),
-                    new JProperty("exerciseID", ((ExerciseRecord)o).getExerciseId()),
-                    new JProperty("weight",((ExerciseRecord)o).getWeight()),
-                    new JProperty("date",((ExerciseRecord)o).getRecordDate().ToString(DATE_FORMAT))
-                    );
+            return exList;
+        }
+        public ExerciseRecord[] GetExerciseRecordList() {
+            object[] list = GetRecordList(EXERCISE_RECORD_LIST);
+            if (list == null) { return new ExerciseRecord[0]; }
+            ExerciseRecord[] exrList = new ExerciseRecord[list.Length];
+            for (int i = 0; i < list.Length; i++) {
+                exrList[i] = (ExerciseRecord)list[i];
             }
-            else if (o is CalorieRecord) {
-                return new JObject(
-                    new JProperty("id", ((CalorieRecord)o).getId()),
-                    new JProperty("calories",((CalorieRecord)o).getCalories()),
-                    new JProperty("date",((CalorieRecord)o).getRecordDate().ToString(DATE_FORMAT))
-                    );
+            return exrList;
+        }
+        public ExerciseRecord[] GetExerciseRecordList(int exID) {
+            object[] list = GetRecordList(EXERCISE_RECORD_LIST, exID);
+            if (list == null) { return new ExerciseRecord[0]; }
+            ExerciseRecord[] exrList = new ExerciseRecord[list.Length];
+            for (int i = 0; i < list.Length; i++) {
+                exrList[i] = (ExerciseRecord)list[i];
             }
-            else if (o is BodyweightRecord) {
-                return new JObject(
-                    new JProperty("id",((BodyweightRecord)o).getId()),
-                    new JProperty("weight",((BodyweightRecord)o).getWeight()),
-                    new JProperty("date", ((BodyweightRecord)o).getRecordDate().ToString(DATE_FORMAT))
-                    );
+            return exrList;
+        }
+        public CalorieRecord[] GetCalorieRecordList() {
+            object[] list = GetRecordList(CALORIE_RECORD_LIST);
+            if (list == null) { return new CalorieRecord[0]; }
+            CalorieRecord[] crList = new CalorieRecord[list.Length];
+            for (int i = 0; i < list.Length; i++) {
+                crList[i] = (CalorieRecord)list[i];
             }
-            return null;
+            return crList;
+        }
+        public CalorieRecord[] GetCalorieRecordList(DateTime d) {
+            object[] list = GetRecordList(CALORIE_RECORD_LIST,d);
+            if (list == null) { return new CalorieRecord[0]; }
+            CalorieRecord[] crList = new CalorieRecord[list.Length];
+            for (int i = 0; i < list.Length; i++) {
+                crList[i] = (CalorieRecord)list[i];
+            }
+            return crList;
+        }
+        public BodyweightRecord[] GetBodyweightRecordList() {
+            object[] list = GetRecordList(BODYWEIGHT_RECORD_LIST);
+            if (list == null) { return new BodyweightRecord[0]; }
+            BodyweightRecord[] bwrList = new BodyweightRecord[list.Length];
+            for (int i = 0; i < list.Length; i++) {
+                bwrList[i] = (BodyweightRecord)list[i];
+            }
+            return bwrList;
+        }
+
+        private object[] GetRecordList(string rList) {
+            try {
+                string getUrl = "/users/"+username+"/"+password+"/"+rList;
+                HttpWebRequest req = establishRequest(getUrl, WebRequestMethods.Http.Get);
+                HttpWebResponse res = (HttpWebResponse)req.GetResponse();
+                if (resolveStatusCode(res)) {
+                    string data = getResponseData(res);
+                    JArray jData = JArray.Parse(data);
+                    object[] recordArray = JArrayToObjectArray(jData,rList);
+                    return recordArray;
+                }
+            } catch (Exception e) { Console.WriteLine(e.ToString()); }
+            return new object[0];
+        }
+        
+        private object[] GetRecordList(string rListType, int fKey) {
+            try {
+                string getUrl = "/users/" + username + "/" + password + "/" + rListType+"/exID/"+fKey;
+                HttpWebRequest req = establishRequest(getUrl, WebRequestMethods.Http.Get);
+                HttpWebResponse res = (HttpWebResponse)req.GetResponse();
+                if (resolveStatusCode(res)) {
+                    string data = getResponseData(res);
+                    JArray jData = JArray.Parse(data);
+                    object[] recordArray = JArrayToObjectArray(jData,rListType);
+                    return recordArray;
+                }
+            } catch (Exception e) { Console.WriteLine(e.ToString()); }
+            return new object[0];
+        }
+        private object[] GetRecordList(string rListType, DateTime d) {
+            try {
+                string getUrl = "/users/" + username + "/" + password + "/" + rListType + "/" + d.ToString(DATE_FORMAT);
+                HttpWebRequest req = establishRequest(getUrl, WebRequestMethods.Http.Get);
+                HttpWebResponse res = (HttpWebResponse)req.GetResponse();
+                if (resolveStatusCode(res)) {
+                    string data = getResponseData(res);
+                    JArray jData = JArray.Parse(data);
+                    object[] recordArray = JArrayToObjectArray(jData,rListType);
+                    return recordArray;
+                }
+            } catch (Exception e) { Console.WriteLine(e.ToString()); }
+            return new object[0];
+        }
+        private object[] JArrayToObjectArray(JArray arr,string rList) {
+            try {
+                object[] objArr = new object[arr.Count];
+                for (int i = 0; i < objArr.Length; i++) {
+                    JObject jObj = (JObject)arr.Children().ElementAt(i);
+                    
+                    Console.WriteLine(jObj);
+                    int id = (int)jObj.GetValue("id");
+                    string dateStr = jObj.GetValue("date").ToString();
+                    DateTime date = DateTime.Parse(dateStr);
+                    
+                    switch (rList) {
+                        case EXERCISE_LIST:
+                            string name = jObj.GetValue("name").ToString();
+                            objArr[i] = new Exercise(id,name,date);
+                            break;
+                        case EXERCISE_RECORD_LIST:
+                            int exID = (int)jObj.GetValue("exerciseID");
+                            double weight = (double)jObj.GetValue("weight");
+                            objArr[i] = new ExerciseRecord(id,exID,weight,date);
+                            break;
+                        case CALORIE_RECORD_LIST:
+                            int calories = (int)jObj.GetValue("calories");
+                            objArr[i] = new CalorieRecord(id,calories,date);
+                            break;
+                        case BODYWEIGHT_RECORD_LIST:
+                            double bWeight = (double)jObj.GetValue("weight");
+                            break;
+                    }
+                    return objArr;
+                }
+            } catch (Exception e) { Console.WriteLine(e.ToString()); }
+            return new object[0];
         }
 
         static void Main() {
             DatabaseController dc = new DatabaseController();
             //Trace.Assert(dc.AddUser("test1","pass"),"Failed to add user!");
-            bool loggedIn = dc.Login("test1","pass");
+            bool loggedIn = dc.Login("user1","p123");
             Trace.Assert(loggedIn == true,"Failed to login");
             //Trace.Assert(dc.UpdatePassword("pass","pass"),"Failed to update password");
             //Trace.Assert(dc.RemoveUser(),"Failed to remove user");
-            Exercise ex = new Exercise(1,"Dumbell Shrug",DateTime.Now);
-            Trace.Assert(dc.AddRecord(EXERCISE,ex),"Failed to add record");
-
-            
+            //Exercise ex = new Exercise(1,"Dumbell Shrug",DateTime.Now);
+            //Trace.Assert(dc.AddRecord(EXERCISE,ex),"Failed to add record");
+            //ex.setName("Shoulder Press");
+            //Trace.Assert(dc.EditRecord(EXERCISE,ex),"Failed to edit record");
+            //Trace.Assert(dc.DeleteRecord(EXERCISE,ex),"Failed to remove record");
+            Trace.Assert(dc.GetExerciseList().Length != 0,"Exercise list length should be greater than 0");
+            Trace.Assert(dc.GetExerciseRecordList().Length != 0, "Exercise Record list length should be greater than 0");
+            Trace.Assert(dc.GetBodyweightRecordList().Length != 0, "Bodyweight record list should be greater than 0");
         }
     }
     
---
+++ FitnessApp/ModelController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessApp
+{
+    class ModelController
+    {
+        private DatabaseController dc;
+        private string username;
+        private string password;
+        public ModelController() {
+            dc = new DatabaseController();
+        }
+        public bool UserSignOut() { return false; }
+        public bool UserSignIn() { return false; }
+
+        public bool CreateBodyweightRecord(double weight) { return false; }
+        public bool CreateCalorieRecord(int calories) { return false; }
+        public bool CreateExercise(string name) { return false; }
+        public bool CreateExerciseRecord(string exName, double weight) { return false; }
+
+        public bool DeleteBodyweightRecord(object o) { return false; }
+        public bool DeleteCalorieRecord(object o) { return false; }
+        public bool DeleteExercise(string exName) { return false; }
+        public bool DeleteExerciseRecord(object o) { return false; }
+
+        public double[] GetBWRWeightList() { return new double[0]; }
+        public object[] GetBodyWeightRecordList() { return new object[0]; }
+        public object[] GetCalorieRecordList() { return new object[0]; }
+        public int GetDailyCalories() { return 0; }
+        public double[] GetERWeightList() { return new double[0]; }
+        public string[] GetExerciseNameList() { return new string[0]; }
+        public object[] GetExerciseRecordList(string name) { return new object[0]; }
+
+
+
+    }
+}
